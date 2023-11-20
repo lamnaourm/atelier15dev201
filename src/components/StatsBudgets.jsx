aab696cb407022bf3845c6f175d71564a7c5704a
lamnaourm <mohammed.lamnaour@gmail.com>
2023-11-20 15:06:45 +0100

    ok

--- src/components/StatsBudgets.jsx
+++ src/components/StatsBudgets.jsx
@@ -6,21 +6,21 @@ export default function StatsBudgets() {
     const budget = useSelector(state => state.budget)
     const depenses = useSelector(state => state.depenses)
 
-    const som = depenses.reduce((s,dep) => s+parseFloat(dep.price), 0)
-  return (
-    <div className='stats'>
-        <div>
-            BUDGET : {budget}
-        </div>
+    const som = depenses.reduce((s, dep) => s + parseFloat(dep.price), 0)
+    return (
+        <div className='stats'>
+            <div>
+                BUDGET : {budget}
+            </div>
 
-        <div>
-            DEPENSES : {som}
-        </div>
+            <div>
+                DEPENSES : {som}
+            </div>
 
-        <div>
-            ECART : {budget - som}
-        </div>
+            <div>
+                ECART : {budget - som}
+            </div>
 
-    </div>
-  )
+        </div>
+    )
 }
